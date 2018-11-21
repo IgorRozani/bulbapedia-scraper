3bd6380b97254a9c19ba310fe7ab37158e42a633
Igor Rozani <igorrozani@gmail.com>
2018-11-20 22:03:32 -0200

    Shellos and Unow Evolution list

--- BulbapediaScraper.Runner/Program.cs
+++ BulbapediaScraper.Runner/Program.cs
@@ -1,4 +1,4 @@
-using BulbapediaScraper.Runner.Enums.EvolutionList;
+﻿using BulbapediaScraper.Runner.Enums.EvolutionList;
 using BulbapediaScraper.Runner.Enums.PokemonList;
 using BulbapediaScraper.Runner.Extensions;
 using BulbapediaScraper.Runner.Models;
@@ -6,6 +6,7 @@ using HtmlAgilityPack;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 
 namespace BulbapediaScraper.Runner
 {
@@ -123,25 +124,57 @@ namespace BulbapediaScraper.Runner
                     if (rollCollumns == null || rollCollumns.Count == 0)
                         continue;
 
-                    if (rollCollumns.Count == 8 || rollCollumns.Count == 6)
+                    if (rollCollumns.Count >= 6 && rollCollumns.Count <= 8)
                     {
                         if (rollCollumns[0].SelectSingleNode("a/img") != null)
                         {
                             var pokemon1Name = rollCollumns.GetByIndex(ThreeOrTwoEvolutionsRowIndex.Pokemon1Name).SelectSingleNode("a/span").InnerText;
-                            var condition1 = rollCollumns.GetByIndex(ThreeOrTwoEvolutionsRowIndex.ConditionEvolution1).InnerText.RemoveSpecialCharacter();
-                            var pokemon2Name = rollCollumns.GetByIndex(ThreeOrTwoEvolutionsRowIndex.Pokemon2Name).SelectSingleNode("a/span").InnerText;
 
-                            pokemon1 = pokemonList.FirstOrDefault(p => p.Name == pokemon1Name);
-                            pokemon2 = pokemonList.FirstOrDefault(p => p.Name == pokemon2Name);
-                            pokemon1.Evolutions.Add(new Evolution(condition1, pokemon2));
-
-                            if (rollCollumns.Count == 8)
+                            if (pokemon1Name.Contains("Unown"))
                             {
-                                var pokemon3Name = rollCollumns.GetByIndex(ThreeOrTwoEvolutionsRowIndex.Pokemon3Name).SelectSingleNode("a/span").InnerText;
-                                var condition2 = rollCollumns.GetByIndex(ThreeOrTwoEvolutionsRowIndex.ConditionEvolution2).InnerText.RemoveSpecialCharacter();
+                                var form1Picture = rollCollumns.GetByIndex(ThreeOrTwoEvolutionsRowIndex.Pokemon1Image).SelectSingleNode("a/img").Attributes["src"].Value.Trim('/');
+                                var form1Name = new StringBuilder();
+                                foreach (var element in rollCollumns.GetByIndex(ThreeOrTwoEvolutionsRowIndex.Pokemon1Name).SelectNodes("a/span"))
+                                    form1Name.Append(" ").Append(element.InnerText);
 
-                                var pokemon3 = pokemonList.FirstOrDefault(p => p.Name == pokemon3Name);
-                                pokemon2.Evolutions.Add(new Evolution(condition2, pokemon3));
+
+                                var form2Picture = rollCollumns.GetByIndex(ThreeOrTwoEvolutionsRowIndex.Pokemon2Image).SelectSingleNode("a/img").Attributes["src"].Value.Trim('/');
+                                var form2Name = new StringBuilder();
+                                foreach (var element in rollCollumns.GetByIndex(ThreeOrTwoEvolutionsRowIndex.Pokemon2Name).SelectNodes("a/span"))
+                                    form2Name.Append(" ").Append(element.InnerText);
+
+                                pokemon1 = pokemonList.FirstOrDefault(p => p.Name == "Unown");
+                                pokemon1.Formes.Add(new Forme(form1Name.ToString().Trim(), form1Picture));
+                                pokemon1.Formes.Add(new Forme(form2Name.ToString().Trim(), form2Picture));
+
+                                if (rollCollumns.Count == 8)
+                                {
+                                    var form3Picture = rollCollumns.GetByIndex(ThreeOrTwoEvolutionsRowIndex.Pokemon3Image).SelectSingleNode("a/img").Attributes["src"].Value.Trim('/');
+                                    var form3Name = new StringBuilder();
+                                    foreach (var element in rollCollumns.GetByIndex(ThreeOrTwoEvolutionsRowIndex.Pokemon3Name).SelectNodes("a/span"))
+                                        form3Name.Append(" ").Append(element.InnerText);
+                                    pokemon1.Formes.Add(new Forme(form3Name.ToString().Trim(), form3Picture));
+                                }
+                            }
+                            else
+                            {
+                                var condition1 = rollCollumns.GetByIndex(ThreeOrTwoEvolutionsRowIndex.ConditionEvolution1).InnerText.RemoveSpecialCharacter();
+                                var pokemon2Name = rollCollumns.GetByIndex(ThreeOrTwoEvolutionsRowIndex.Pokemon2Name).SelectSingleNode("a/span").InnerText;
+
+                                pokemon1 = pokemonList.FirstOrDefault(p => p.Name == pokemon1Name);
+                                pokemon2 = pokemonList.FirstOrDefault(p => p.Name == pokemon2Name);
+                                pokemon1.Evolutions.Add(new Evolution(condition1, pokemon2));
+
+                                if (rollCollumns.Count == 8)
+                                {
+                                    var pokemon3Name = rollCollumns.GetByIndex(ThreeOrTwoEvolutionsRowIndex.Pokemon3Name).SelectSingleNode("a/span").InnerText;
+                                    var condition2 = rollCollumns.GetByIndex(ThreeOrTwoEvolutionsRowIndex.ConditionEvolution2).InnerText.RemoveSpecialCharacter();
+
+                                    var pokemon3 = pokemonList.FirstOrDefault(p => p.Name == pokemon3Name);
+                                    pokemon2.Evolutions.Add(new Evolution(condition2, pokemon3));
+                                }
+
+
                             }
                         }
                         else
