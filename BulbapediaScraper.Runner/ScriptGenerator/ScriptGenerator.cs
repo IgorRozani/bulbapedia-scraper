f5ccd8450970e0d870f88f64384d81ec44a21782
Igor Rozani <igorrozani@gmail.com>
2019-04-18 18:11:45 -0300

    Script node type

--- BulbapediaScraper.Runner/ScriptGenerator/ScriptGenerator.cs
+++ BulbapediaScraper.Runner/ScriptGenerator/ScriptGenerator.cs
@@ -1,6 +1,7 @@
 ﻿using BulbapediaScraper.Runner.Models;
 using System.Collections.Generic;
 using System.Text;
+using System.Linq;
 
 namespace BulbapediaScraper.Runner.ScriptGenerator
 {
@@ -8,11 +9,28 @@ namespace BulbapediaScraper.Runner.ScriptGenerator
     {
         public string Generate(ICollection<Pokemon> pokemons)
         {
-            var script = new StringBuilder();
+            var neo4jGenerator = new Neo4jGenerator();
 
+            var script = new StringBuilder();
             script.AppendLine("CREATE");
+            var scriptTypes = new StringBuilder();
+            scriptTypes.AppendLine("CREATE");
+
+            var types = new List<Type>();
+
+            
             foreach(var pokemon in pokemons)
             {
+                foreach(var type in pokemon.Types)
+                {
+                    if (types.All(t => t.Name != type.Name))
+                    {
+                        types.Add(type);
+                        scriptTypes.AppendLine($"({type.Name}:Type) {{ name: '{type.Name}'}}),");
+                    }
+                        
+
+                }
                 script.AppendLine($"({pokemon.GetCleanName()}:Pokemon {{ name:\"{pokemon.Name}\", nationalPokedexNumber:{pokemon.NationalPokedexNumber}, regionalPokedexNumber:'{pokemon.RegionalPokedexNumber}', picture:'{pokemon.Picture}', profileUrl:'{pokemon.ProfileUrl}' }}),");
             }
 
